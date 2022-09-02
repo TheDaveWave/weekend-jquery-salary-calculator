5602e80ba7ed16f683d818f265dd22d8e45bfe75
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-02 16:47:15 -0500

    created a function to create an employee object given arguments

--- scripts/client.js
+++ scripts/client.js
@@ -1,2 +1,17 @@
 /// <reference path="jQuery.js" />
 
+$(readyNow);
+
+function readyNow () {
+
+}
+
+let employees = [];
+
+// function to create an employee object, push it into the array of employees, 
+// and return the employee object.
+function createEmployee (firstName, lastName, idNumber, jobTitle, annualSalary) {
+    const employee = {firstName,lastName,idNumber,jobTitle, annualSalary};
+    employees.push(employee);
+    return employee;
+}
