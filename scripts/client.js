bd43cfadd8c93bc27b1558b3f2330b4c78874498
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-03 10:26:31 -0500

    deleted test employee code comment

--- scripts/client.js
+++ scripts/client.js
@@ -10,19 +10,6 @@ function readyNow () {
 // declare an empty array to store employee objects.
 let employees = [];
 
-// create a test employee object.
-// const testEmployee = {
-//     firstName: 'Dude',
-//     lastName: 'Slice',
-//     idNumber: 666,
-//     jobTitle: 'Surfer',
-//     annualSalary: 5000
-// };
-
-// // push the testEmployee into the employees array.
-// employees.push(testEmployee);
-// console.log(employees);
-
 // function to create an employee object, push it into the array of employees, 
 // and return the employee object.
 function createEmployee (firstName, lastName, idNumber, jobTitle, annualSalary) {
