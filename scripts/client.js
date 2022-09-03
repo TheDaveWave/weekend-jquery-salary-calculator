5c46f0502d7a7f2257483f15051ad6e86720c943
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-03 09:58:18 -0500

    created a test employee

--- scripts/client.js
+++ scripts/client.js
@@ -6,7 +6,21 @@ function readyNow () {
 
 }
 
+// declare an empty array to store employee objects.
 let employees = [];
+
+// create a test employee object.
+const testEmployee = {
+    firstName: 'Dude',
+    lastName: 'Slice',
+    idNumber: 666,
+    jobTitle: 'Surfer',
+    annualSalary: 5000
+};
+
+// push the testEmployee into the employees array.
+employees.push(testEmployee);
+console.log(employees);
 
 // function to create an employee object, push it into the array of employees, 
 // and return the employee object.
